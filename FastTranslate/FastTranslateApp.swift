7e2556d0f57dcd31d42f11e98a03029ed57580ca
Siarhei Luk <siarhei.luk@innowise-group.com>
2026-04-08 16:28:58 +0200

    Initial Commit

---
+++ FastTranslate/FastTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTranslateApp.swift
+//  FastTranslate
+//
+//  Created by Sergey Luk on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct FastTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
